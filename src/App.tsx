2e51e31e786bbdbb7a53a3c6de33a9130a1ddb67
Jenna Palmer <jenna.palmer@tmtinsights.com>
2023-08-02 15:36:42 -0700

    made random button that places the knight somewhere random on the board

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Board from "./components/Board";
+import { Button } from "@mui/material";
+
+// generate random position
+export function observe(): [number, number] {
+  const randPos = () => Math.floor(Math.random() * 8);
+  return [randPos(), randPos()];
+}
 
 function App() {
+  const [knightPosition, setKnightPosition] = useState<[number, number]>([
+    0, 0,
+  ]);
   // CHESS BOARD
   return (
     <div className="App">
-      <Board knightPosition={[0, 0]} />
+      <div style={{ width: "100%", background: "#42a5f58a" }}>
+        <Button
+          variant="contained"
+          sx={{ margin: 1 }}
+          onClick={() => setKnightPosition(observe())}
+        >
+          Randomize Position
+        </Button>
+      </div>
+      <Board {...{ knightPosition }} />
     </div>
   );
 }
